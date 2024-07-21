075a7dbdf9b0aab9a6d7f97df97cb576c727a9b4
hwangjeyeon <kingpele9018@gmail.com>
2024-07-21 19:00:17 +0900

    init commit

---
+++ src/main/java/trendravel/photoravel_be/PhotoravelBeApplication.java
@@ -0,0 +1,13 @@
+package trendravel.photoravel_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoravelBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoravelBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/trendravel/photoravel_be/PhotoravelBeApplicationTests.java
@@ -0,0 +1,13 @@
+package trendravel.photoravel_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoravelBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
