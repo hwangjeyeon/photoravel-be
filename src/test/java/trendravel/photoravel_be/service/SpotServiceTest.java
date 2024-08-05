7522d7e752d5c10c05bdf088e0b1773aeb09935e
hwangjeyeon <kingpele9018@gmail.com>
2024-08-06 00:10:51 +0900

    Test: 특정 Spot Read 기능 동작 테스트

--- src/test/java/trendravel/photoravel_be/service/SpotServiceTest.java
+++ src/test/java/trendravel/photoravel_be/service/SpotServiceTest.java
@@ -1,10 +1,19 @@
 package trendravel.photoravel_be.service;
 
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import trendravel.photoravel_be.commom.service.ImageService;
+import trendravel.photoravel_be.db.respository.review.ReviewRepository;
+import trendravel.photoravel_be.db.review.Review;
+import trendravel.photoravel_be.db.review.enums.ReviewTypes;
+import trendravel.photoravel_be.db.spot.Spot;
+import trendravel.photoravel_be.domain.review.dto.response.RecentReviewsDto;
+import trendravel.photoravel_be.domain.review.service.ReviewService;
+import trendravel.photoravel_be.domain.spot.dto.response.SpotSingleReadResponseDto;
 import trendravel.photoravel_be.domain.spot.service.SpotService;
 import trendravel.photoravel_be.domain.spot.dto.request.SpotRequestDto;
 import trendravel.photoravel_be.db.location.Location;
@@ -12,16 +21,24 @@ import trendravel.photoravel_be.db.respository.location.LocationRepository;
 import trendravel.photoravel_be.db.respository.spot.SpotRepository;
 
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataJpaTest
+@SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Transactional
 class SpotServiceTest {
 
     @Autowired
     LocationRepository locationRepository;
     @Autowired
     SpotRepository spotRepository;
+    @Autowired
+    ReviewRepository reviewRepository;
+
+    @Autowired
+    EntityManager em;
 
     @MockBean
     SpotService spotService;
@@ -31,6 +48,13 @@ class SpotServiceTest {
 
     SpotRequestDto spotRequestDto;
     Location location;
+    Spot spot;
+    Review review1;
+    Review review2;
+    Review review3;
+    Review review4;
+    @Autowired
+    private ReviewService reviewService;
 
     @BeforeEach
     void before(){
@@ -46,6 +70,54 @@ class SpotServiceTest {
                 .views(0)
                 .build();
         locationRepository.save(location);
+        spot = Spot
+                .builder()
+                .description("미디어랩스관입니다")
+                .longitude(35.24)
+                .latitude(46.61)
+                .title("미디어랩스건물 방문")
+                .views(0)
+                .location(location)
+                .build();
+        spot.setLocation(location);
+        spotRepository.save(spot);
+        review1 = Review
+                .builder()
+                .reviewType(ReviewTypes.LOCATION)
+                .content("멋지네")
+                .rating(1.5)
+                .locationReview(location)
+                .build();
+        review2 = Review
+                .builder()
+                .reviewType(ReviewTypes.LOCATION)
+                .content("키야")
+                .rating(2.4)
+                .locationReview(location)
+                .build();
+        review3 = Review
+                .builder()
+                .reviewType(ReviewTypes.LOCATION)
+                .content("이야")
+                .rating(3.42)
+                .locationReview(location)
+                .build();
+        review4 = Review
+                .builder()
+                .reviewType(ReviewTypes.LOCATION)
+                .content("그저 굿")
+                .rating(4.5)
+                .locationReview(location)
+                .build();
+        review1.setLocationReview(location);
+        review2.setLocationReview(location);
+        review3.setLocationReview(location);
+        review4.setLocationReview(location);
+
+        reviewRepository.save(review1);
+        reviewRepository.save(review2);
+        reviewRepository.save(review3);
+        reviewRepository.save(review4);
 
         spotRequestDto.setSpotId(1L);
         spotRequestDto.setTitle("미디어랩스건물 방문");
@@ -116,6 +188,46 @@ class SpotServiceTest {
         spotService.deleteSpot(spotRequestDto.getSpotId());
 
         assertThat(spotRepository.findById(spotRequestDto.getSpotId())).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Spot SINGLE READ 서비스가 잘 동작하는지 테스트")
+    void readSingleSpotServiceTest(){
+        //given
+        spotService.readSingleSpot(location.getId(), spot.getId());
+        Spot findSpot = spotRepository.findById(spot.getId()).orElse(null);
+
+        //when
+        SpotSingleReadResponseDto spotSingleReadResponseDto
+                = spotService.readSingleSpot(location.getId(), spot.getId());
+        List<RecentReviewsDto> findRecentReviews =
+                spotSingleReadResponseDto.getRecentReviewDtos();
+
+        //then
+
+        assertThat(spotSingleReadResponseDto.getSpotId())
+                .isEqualTo(findSpot.getId());
+        assertThat(spotSingleReadResponseDto.getLatitude())
+                .isEqualTo(findSpot.getLatitude());
+        assertThat(spotSingleReadResponseDto.getLongitude())
+                .isEqualTo(findSpot.getLongitude());
+        assertThat(spotSingleReadResponseDto.getDescription())
+                .isEqualTo(findSpot.getDescription());
+        assertThat(spotSingleReadResponseDto.getTitle())
+                .isEqualTo(findSpot.getTitle());
+        assertThat(spotSingleReadResponseDto.getRatingAvg())
+                .isEqualTo(String.format("%.2f",
+                        (review4.getRating() + review2.getRating()
+                                + review3.getRating()) / 3));
+        assertThat(spotSingleReadResponseDto.getCreatedTime())
+                .isEqualTo(findSpot.getCreatedAt());
+        assertThat(spotSingleReadResponseDto.getUpdatedTime())
+                .isEqualTo(findSpot.getUpdatedAt());
+        assertThat(spotSingleReadResponseDto.getViews()).isEqualTo(0);
+        assertThat(findRecentReviews.size()).isGreaterThan(1);
+        assertThat(findRecentReviews).extracting("rating")
+                .containsExactlyInAnyOrder(review4.getRating(),
+                        review2.getRating(), review3.getRating());
     }
 
 }
