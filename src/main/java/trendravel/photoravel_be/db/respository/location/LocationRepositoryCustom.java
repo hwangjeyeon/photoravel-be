c1988ab3ea276feda829f2ab96ab956b35476964
hwangjeyeon <kingpele9018@gmail.com>
2024-08-05 23:49:55 +0900

    Fix: review 데이터를 조회하지 못하던 문제 해결

--- src/main/java/trendravel/photoravel_be/db/respository/location/LocationRepositoryCustom.java
+++ src/main/java/trendravel/photoravel_be/db/respository/location/LocationRepositoryCustom.java
@@ -1,12 +1,11 @@
 package trendravel.photoravel_be.db.respository.location;
 
-import trendravel.photoravel_be.db.location.Location;
 import trendravel.photoravel_be.domain.review.dto.response.RecentReviewsDto;
 
 import java.util.List;
 
 public interface LocationRepositoryCustom {
 
-    List<RecentReviewsDto> recentReviews(Location location);
+    List<RecentReviewsDto> recentReviews();
 
 }
--- src/main/java/trendravel/photoravel_be/db/respository/location/LocationRepositoryImpl.java
+++ src/main/java/trendravel/photoravel_be/db/respository/location/LocationRepositoryImpl.java
@@ -1,34 +1,34 @@
 package trendravel.photoravel_be.db.respository.location;
 
-import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import trendravel.photoravel_be.db.location.Location;
+import lombok.RequiredArgsConstructor;
+import trendravel.photoravel_be.db.review.Review;
 import trendravel.photoravel_be.domain.review.dto.response.RecentReviewsDto;
+
 
 import java.util.List;
 
+
+import static trendravel.photoravel_be.db.location.QLocation.location;
 import static trendravel.photoravel_be.db.review.QReview.review;
 
+@RequiredArgsConstructor
 public class LocationRepositoryImpl implements LocationRepositoryCustom{
 
     private final JPAQueryFactory queryFactory;
 
-    public LocationRepositoryImpl(EntityManager em) {
-        this.queryFactory = new JPAQueryFactory(em);
-    }
-
     @Override
-    public List<RecentReviewsDto> recentReviews(Location location) {
-        return queryFactory
-                .select(Projections.fields(RecentReviewsDto.class,
-                        review.content,
-                        review.rating,
-                        review.images))
+    public List<RecentReviewsDto> recentReviews() {
+        List<Review> recentReviews = queryFactory
+                .select(review)
                 .from(review)
-                .where(review.locationReview.id.eq(location.getId()))
+                .innerJoin(review.locationReview, location)
                 .orderBy(review.updatedAt.desc())
                 .limit(3)
                 .fetch();
+
+        return recentReviews.stream()
+                .map(p -> new RecentReviewsDto(p.getContent(), p.getRating(), p.getImages()))
+                .toList();
     }
 }
