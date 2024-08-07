b7a78fd0d996ad396206692b7e235c9be3e5f67f
hwangjeyeon <kingpele9018@gmail.com>
2024-08-08 00:00:11 +0900

    Feat: 현재 위치 기반 장소 요청 Dto 기본 생성자 추가

--- src/main/java/trendravel/photoravel_be/domain/location/dto/request/LocationNowPositionDto.java
+++ src/main/java/trendravel/photoravel_be/domain/location/dto/request/LocationNowPositionDto.java
@@ -3,9 +3,11 @@ package trendravel.photoravel_be.domain.location.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class LocationNowPositionDto {
 
     private double latitude;
