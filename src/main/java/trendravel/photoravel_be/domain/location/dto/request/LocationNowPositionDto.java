5839bac4e76a4cc81f647b2dc172dbac7e716a91
hwangjeyeon <kingpele9018@gmail.com>
2024-08-07 23:35:09 +0900

    Feat: 파라미터를 담기 위한 생성자 애노테이션 추가

--- src/main/java/trendravel/photoravel_be/domain/location/dto/request/LocationNowPositionDto.java
+++ src/main/java/trendravel/photoravel_be/domain/location/dto/request/LocationNowPositionDto.java
@@ -1,9 +1,11 @@
 package trendravel.photoravel_be.domain.location.dto.request;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class LocationNowPositionDto {
 
     private double latitude;
