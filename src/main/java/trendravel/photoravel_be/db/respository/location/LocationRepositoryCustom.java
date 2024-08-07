9b476442142452166a48786402cdd1d03b3be684
hwangjeyeon <kingpele9018@gmail.com>
2024-08-07 22:41:03 +0900

    Feat: 현재 위치 기반 주변 장소 READ 기능 구현
    - 약간의 오차범위 존재

--- src/main/java/trendravel/photoravel_be/db/respository/location/LocationRepositoryCustom.java
+++ src/main/java/trendravel/photoravel_be/db/respository/location/LocationRepositoryCustom.java
@@ -1,5 +1,7 @@
 package trendravel.photoravel_be.db.respository.location;
 
+import trendravel.photoravel_be.db.location.Location;
+import trendravel.photoravel_be.domain.location.dto.request.LocationNowPositionDto;
 import trendravel.photoravel_be.domain.review.dto.response.RecentReviewsDto;
 
 import java.util.List;
@@ -7,5 +9,6 @@ import java.util.List;
 public interface LocationRepositoryCustom {
 
     List<RecentReviewsDto> recentReviews(Long id);
+    List<Location> searchNowPosition(LocationNowPositionDto locationNowPositionDto);
 
 }
--- src/main/java/trendravel/photoravel_be/db/respository/location/LocationRepositoryImpl.java
+++ src/main/java/trendravel/photoravel_be/db/respository/location/LocationRepositoryImpl.java
@@ -1,14 +1,22 @@
 package trendravel.photoravel_be.db.respository.location;
 
+import com.querydsl.core.types.dsl.BooleanTemplate;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+import trendravel.photoravel_be.db.location.Location;
 import trendravel.photoravel_be.db.review.Review;
+import trendravel.photoravel_be.domain.location.dto.request.LocationNowPositionDto;
 import trendravel.photoravel_be.domain.review.dto.response.RecentReviewsDto;
 
 
 import java.util.List;
 
 
+import static java.util.Objects.isNull;
 import static trendravel.photoravel_be.db.location.QLocation.location;
 import static trendravel.photoravel_be.db.review.QReview.review;
 
@@ -17,6 +25,10 @@ public class LocationRepositoryImpl implements LocationRepositoryCustom{
 
     private final JPAQueryFactory queryFactory;
 
+
+    /**
+     * 성능 최적화를 위한 fetch join 리팩토링 고민할 것
+     */
     @Override
     public List<RecentReviewsDto> recentReviews(Long id) {
         List<Review> recentReviews = queryFactory
@@ -29,7 +41,35 @@ public class LocationRepositoryImpl implements LocationRepositoryCustom{
                 .fetch();
 
         return recentReviews.stream()
-                .map(p -> new RecentReviewsDto(p.getContent(), p.getRating(), p.getImages()))
+                .map(p -> new RecentReviewsDto(p.getContent(),
+                        p.getRating(), p.getImages()))
                 .toList();
     }
+
+    @Override
+    public List<Location> searchNowPosition(
+            LocationNowPositionDto locationNowPositionDto) {
+        return queryFactory
+                .select(location)
+                .from(location)
+                .where(inRangeDistance(locationNowPositionDto))
+                .fetch();
+
+    }
+
+    private BooleanTemplate inRangeDistance(
+            LocationNowPositionDto locationNowPositionDto) {
+        Point points = new GeometryFactory()
+                .createPoint(new Coordinate(locationNowPositionDto.getLatitude()
+                        , locationNowPositionDto.getLongitude()));
+
+        if(isNull(points)){
+            return null;
+        }
+        points.setSRID(4326);
+
+        return Expressions.booleanTemplate("ST_Contains(ST_BUFFER({0}, {1}), {2})",
+                points, locationNowPositionDto.getRange(), location.point);
+    }
+
 }
