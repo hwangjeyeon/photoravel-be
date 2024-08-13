2126b5454ffa4c753da24d3ff3eb2f7b2a19533b
DongUk <az9170@naver.com>
2024-08-13 15:00:20 +0900

    Feat: 가이드북 요청, 응답 DTO 구현

---
+++ src/main/java/trendravel/photoravel_be/domain/guidebook/dto/request/GuidebookRequestDto.java
@@ -0,0 +1,23 @@
+package trendravel.photoravel_be.domain.guidebook.dto.request;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import org.springframework.http.MediaType;
+import trendravel.photoravel_be.db.enums.Region;
+
+@Schema(description = "가이드북 생성 수정 요청 DTO", 
+        contentEncoding = MediaType.APPLICATION_JSON_VALUE)
+@Data
+public class GuidebookRequestDto {
+    
+    @Schema(description = "가이드북 ID")
+    private Long id;
+    @Schema(description = "유저 아이디(FK)")
+    private Long userId;
+    @Schema(description = "제목")
+    private String title;
+    @Schema(description = "내용")
+    private String content;
+    @Schema(description = "지역")
+    private Region region;
+    
+}
