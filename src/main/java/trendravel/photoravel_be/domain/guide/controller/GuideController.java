a93c478526fad8d9f4facef83f5ea2179daaab63
DongUk <az9170@naver.com>
2024-08-15 15:32:22 +0900

    Feat: 가이드, 가이드북 Controller 구현

---
+++ src/main/java/trendravel/photoravel_be/domain/guide/controller/GuideController.java
@@ -0,0 +1,69 @@
+package trendravel.photoravel_be.domain.guide.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import trendravel.photoravel_be.commom.response.Api;
+import trendravel.photoravel_be.commom.response.Result;
+import trendravel.photoravel_be.domain.guide.dto.request.GuideLoginRequestDto;
+import trendravel.photoravel_be.domain.guide.dto.request.GuideRequestDto;
+import trendravel.photoravel_be.domain.guide.dto.response.GuideResponseDto;
+import trendravel.photoravel_be.domain.guide.service.GuideService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/guides")
+@RequiredArgsConstructor
+public class GuideController {
+    
+    private final GuideService guideService;
+    
+    
+    @GetMapping
+    public Api<List<GuideResponseDto>> guidesList(@RequestParam String keyword) {
+
+        return Api.READ(guideService.getGuideList(keyword));
+    }
+    
+    @GetMapping("/{guideId}/detail")
+    public Api<GuideResponseDto> guideDetail(@PathVariable String guideId) {
+
+        return Api.READ(guideService.getGuide(guideId));
+    }
+    
+    @PostMapping("/join")
+    public Result createGuide(
+            @RequestPart(value = "data") GuideRequestDto guideRequestDto,
+            @RequestPart(value = "image", required = false) String image) {
+        
+        guideService.createGuide(guideRequestDto, image);
+        
+        return Result.CREATED();
+    }
+    
+    @PostMapping("/login")
+    public Api<GuideResponseDto> guideLogin(@RequestBody GuideLoginRequestDto loginRequestDto) {
+        
+        return Api.OK(guideService.authenticate(
+                loginRequestDto.getUsername(), 
+                loginRequestDto.getPassword()));
+    }
+    
+    @PatchMapping("/{guideId}/update")
+    public Api<GuideResponseDto> updateGuide(
+            @PathVariable String guideId,
+            @RequestPart(value = "data") GuideRequestDto guideRequestDto,
+            @RequestPart(value = "image", required = false) List<MultipartFile> images) {
+        
+        return Api.UPDATED(guideService.updateGuide(guideId, guideRequestDto, images));
+    }
+    
+    @DeleteMapping("/{guideId}/delete")
+    public Result guidebookDelete(@PathVariable String guideId) {
+        
+        guideService.deleteGuide(guideId);
+        return Result.DELETED();
+    }
+    
+}
---
+++ src/main/java/trendravel/photoravel_be/domain/guidebook/controller/GuidebookController.java
@@ -0,0 +1,92 @@
+package trendravel.photoravel_be.domain.guidebook.controller;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import trendravel.photoravel_be.commom.response.Api;
+import trendravel.photoravel_be.commom.response.Result;
+import trendravel.photoravel_be.domain.guidebook.dto.request.GuidebookRequestDto;
+import trendravel.photoravel_be.domain.guidebook.dto.response.GuidebookResponseDto;
+import trendravel.photoravel_be.db.guidebook.Guidebook;
+import trendravel.photoravel_be.domain.guidebook.service.GuidebookService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/guidebooks")
+@RequiredArgsConstructor
+public class GuidebookController {
+    
+    private final GuidebookService guidebookService;
+    
+    @Schema(description = "가이드북 생성 요청 (이미지 미포함)",
+            contentEncoding = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping("/create")
+    public Api<GuidebookResponseDto> guidebookCreate(
+            @RequestBody GuidebookRequestDto guidebookRequestDto) {
+        
+        return Api.CREATED(guidebookService.createGuidebook(guidebookRequestDto));
+    }
+    
+    @Schema(description = "가이드북 생성 요청 (이미지 포함)",
+            contentEncoding = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PostMapping(value = "/create",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Api<GuidebookResponseDto> guidebookCreate(
+            @RequestPart(value = "data") GuidebookRequestDto guidebookRequestDto,
+            @RequestPart(value = "images", required = false) List<MultipartFile> images) {
+        
+        return Api.CREATED(guidebookService.createGuidebook(guidebookRequestDto, images));
+    }
+    
+    
+    @GetMapping
+    public Api<List<GuidebookResponseDto>> guidebooksList(
+            @RequestParam String region,
+            @RequestParam String keyword) {
+        
+        return Api.READ(guidebookService.getGuidebookList(region, keyword));
+    }
+    
+    @GetMapping("/{guidebookId}/detail")
+    public Api<GuidebookResponseDto> guidebookDetail(@PathVariable Long guidebookId) {
+        
+        return Api.READ(guidebookService.getGuidebook(guidebookId));
+    }
+    
+    @Schema(description = "가이드북 수정 요청 (이미지 미포함)",
+            contentEncoding = MediaType.APPLICATION_JSON_VALUE)
+    @PatchMapping("/{guidebookId}/update")
+    public Api<GuidebookResponseDto> guidebookUpdate(
+            @PathVariable Long guidebookId,
+            @RequestPart(value = "data") GuidebookRequestDto guidebookRequestDto) {
+        
+        return Api.UPDATED(guidebookService.updateGuidebook(guidebookId, guidebookRequestDto));
+    }
+    
+    @Schema(description = "가이드북 수정 요청 (이미지 미포함)",
+            contentEncoding = MediaType.APPLICATION_JSON_VALUE)
+    @PatchMapping(value = "/{guidebookId}/update",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Api<GuidebookResponseDto> guidebookUpdate(
+            @PathVariable Long guidebookId,
+            @RequestPart(value = "data") GuidebookRequestDto guidebookRequestDto,
+            @RequestPart(value = "images", required = false) List<MultipartFile> images) {
+        
+        return Api.UPDATED(guidebookService.updateGuidebook(guidebookId, guidebookRequestDto, images));
+    }
+    
+    @Schema(description = "가이드북 삭제 요청")
+    @DeleteMapping("/{guidebookId}/delete")
+    public Result guidebookDelete(@PathVariable Long guidebookId) {
+        
+        guidebookService.deleteGuidebook(guidebookId);
+        return Result.DELETED();
+    }
+    
+}
