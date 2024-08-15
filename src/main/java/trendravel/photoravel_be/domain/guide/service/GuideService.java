c435f852f7d888e6728eb979061fb648c97d9604
DongUk <az9170@naver.com>
2024-08-15 15:32:11 +0900

    Feat: 가이드, 가이드북 Service 구현

---
+++ src/main/java/trendravel/photoravel_be/domain/guide/service/GuideService.java
@@ -0,0 +1,141 @@
+package trendravel.photoravel_be.domain.guide.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import trendravel.photoravel_be.commom.error.ErrorCode;
+import trendravel.photoravel_be.commom.exception.ApiException;
+import trendravel.photoravel_be.commom.service.ImageService;
+import trendravel.photoravel_be.db.guide.Guide;
+import trendravel.photoravel_be.db.respository.guide.GuideRepository;
+import trendravel.photoravel_be.db.review.Review;
+import trendravel.photoravel_be.domain.guide.dto.request.GuideRequestDto;
+import trendravel.photoravel_be.domain.guide.dto.response.GuideResponseDto;
+import trendravel.photoravel_be.domain.review.dto.response.RecentReviewsDto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class GuideService {
+    
+    private final GuideRepository guideRepository;
+    private final ImageService imageService;
+    
+    @Transactional
+    public List<GuideResponseDto> getGuideList(String keyword) {
+        
+        List<Guide> guides = guideRepository.findByNameContaining(keyword);
+        
+        List<GuideResponseDto> responseDtoList = new ArrayList<>();
+        
+        for (Guide guide : guides) {
+            GuideResponseDto dto = GuideResponseDto.builder()
+                    .accountId(guide.getAccountId())
+                    .region(guide.getRegion())
+                    .ratingAvg(String.format("%.2f", ratingAverage(guide.getReviews())))
+                    .reviewCount(guide.getReviews().size())
+                    .build();
+            
+            responseDtoList.add(dto);
+        }
+        
+        return responseDtoList;
+    }
+    
+    @Transactional
+    public GuideResponseDto getGuide(String guideId) {
+        
+        Optional<Guide> guideOpt = guideRepository.findByAccountId(guideId);
+        if (guideOpt.isEmpty()) {
+            
+        }
+        Guide guide = guideOpt.get();
+        
+        //List<RecentReviewsDto> reviews = guideRepository.recentReviews(guide.getId());
+        
+        return GuideResponseDto.builder()
+                .accountId(guide.getAccountId())
+                .name(guide.getName())
+                .region(guide.getRegion())
+                .description(guide.getDescription())
+                .profileImg(guide.getProfileImg())
+                .createdAt(guide.getCreatedAt())
+                .ratingAvg(String.format("%.2f", ratingAverage(guide.getReviews())))
+                //.recentReviewDtos(reviews)
+                .build();
+    }
+    
+    @Transactional
+    public void createGuide(GuideRequestDto guideRequestDto, String image) {
+        
+        Guide guide = guideRepository.save(Guide.builder()
+                        .accountId(guideRequestDto.getAccountId())
+                        .password(guideRequestDto.getPassword())
+                        .name(guideRequestDto.getName())
+                        .region(guideRequestDto.getRegion())
+                        .description(guideRequestDto.getDescription())
+                        .profileImg(guideRequestDto.getProfileImg())
+                        .build());
+        
+    }
+    @Transactional
+    public GuideResponseDto authenticate(String username, String password) {
+        
+        //인증 로직 필요
+        Optional<Guide> guideOpt = guideRepository.findByAccountId(username);
+        
+        if (guideOpt.isPresent()) {
+            throw new ApiException(ErrorCode.BAD_REQUEST);
+        }
+        
+        return GuideResponseDto.builder()
+                .name(guideOpt.get().getName())
+                .build();
+    }
+    
+    @Transactional
+    public GuideResponseDto updateGuide(String accountId,
+                                        GuideRequestDto guideRequestDto,
+                                        List<MultipartFile> images) {
+        
+        Optional<Guide> guideOpt = guideRepository.findByAccountId(accountId);
+        
+        if (guideOpt.isEmpty()) {
+            
+        }
+        
+        Guide guide = guideOpt.get();
+        guide.updateGuide(guideRequestDto, imageService.uploadImages(images));
+        
+        //List<RecentReviewsDto> reviews = guideRepository.recentReviews(guide.getId());
+        
+        return GuideResponseDto.builder()
+                .accountId(guide.getAccountId())
+                .name(guide.getName())
+                .region(guide.getRegion())
+                .description(guide.getDescription())
+                .profileImg(guide.getProfileImg())
+                .createdAt(guide.getCreatedAt())
+                .ratingAvg(String.format("%.2f", ratingAverage(guide.getReviews())))
+                //.recentReviewDtos(reviews)
+                .build();
+    }
+    @Transactional
+    public void deleteGuide(String guideId) {
+        guideRepository.deleteByAccountId(guideId);
+    }
+    
+    private double ratingAverage(List<Review> reviews) {
+        double sum = 0;
+        for (Review review : reviews) {
+            sum += review.getRating();
+        }
+        return sum / reviews.size();
+    }
+    
+    
+}
---
+++ src/main/java/trendravel/photoravel_be/domain/guidebook/service/GuidebookService.java
@@ -0,0 +1,181 @@
+package trendravel.photoravel_be.domain.guidebook.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import trendravel.photoravel_be.domain.guidebook.dto.request.GuidebookRequestDto;
+import trendravel.photoravel_be.domain.guidebook.dto.response.GuidebookResponseDto;
+import trendravel.photoravel_be.db.guidebook.Guidebook;
+import trendravel.photoravel_be.db.enums.Region;
+import trendravel.photoravel_be.db.respository.guidebook.GuidebookRepository;
+import trendravel.photoravel_be.commom.service.ImageService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class GuidebookService {
+    
+    private final GuidebookRepository guidebookRepository;
+    private final ImageService imageService;
+    
+    @Transactional
+    public GuidebookResponseDto createGuidebook(
+            GuidebookRequestDto guidebookRequestDto, 
+            List<MultipartFile> images) {
+        
+        Guidebook guidebook = guidebookRepository.save(Guidebook.builder()
+                .userId(guidebookRequestDto.getUserId())
+                .title(guidebookRequestDto.getTitle())
+                .content(guidebookRequestDto.getContent())
+                .region(guidebookRequestDto.getRegion())
+                .images(imageService.uploadImages(images))
+                .views(0)
+                .build());
+        
+        
+        return GuidebookResponseDto.builder()
+                .userId(guidebook.getUserId())
+                .title(guidebook.getTitle())
+                .content(guidebook.getContent())
+                .region(guidebook.getRegion())
+                .images(guidebook.getImages())
+                .createdAt(guidebook.getCreatedAt())
+                .updatedAt(guidebook.getUpdatedAt())
+                .views(guidebook.getViews())
+                .build();
+    }
+    
+    @Transactional
+    public GuidebookResponseDto createGuidebook(
+            GuidebookRequestDto guidebookRequestDto) {
+        
+        Guidebook guidebook = guidebookRepository.save(Guidebook.builder()
+                .userId(guidebookRequestDto.getUserId())
+                .title(guidebookRequestDto.getTitle())
+                .content(guidebookRequestDto.getContent())
+                .region(guidebookRequestDto.getRegion())
+                .views(0)
+                .build());
+        
+        return GuidebookResponseDto.builder()
+                .id(guidebook.getId())
+                .userId(guidebook.getUserId())
+                .title(guidebook.getTitle())
+                .content(guidebook.getContent())
+                .region(guidebook.getRegion())
+                .createdAt(guidebook.getCreatedAt())
+                .updatedAt(guidebook.getUpdatedAt())
+                .views(guidebook.getViews())
+                .build();
+    }
+    
+    
+    @Transactional
+    public List<GuidebookResponseDto> getGuidebookList(String region, String keyword) {
+        
+        List<Guidebook> guidebooks;
+        
+        if (keyword == null || keyword.isEmpty()) {
+            guidebooks = guidebookRepository.findByRegion(Region.valueOf(region));
+        } else {
+            guidebooks = guidebookRepository.findByTitleContaining(keyword);
+        }
+        
+        List<GuidebookResponseDto> responseDtoList = new ArrayList<>();
+        
+        for (Guidebook guidebook : guidebooks) {
+            GuidebookResponseDto dto = GuidebookResponseDto.builder()
+                    .id(guidebook.getId())
+                    .userId(guidebook.getUserId())
+                    .title(guidebook.getTitle())
+                    .region(guidebook.getRegion())
+                    .views(guidebook.getViews())
+                    .createdAt(guidebook.getCreatedAt())
+                    .updatedAt(guidebook.getUpdatedAt())
+                    .build();
+            responseDtoList.add(dto);
+        }
+        
+        return responseDtoList;
+    }
+    
+    @Transactional
+    public GuidebookResponseDto getGuidebook(Long guidebookId) {
+        Guidebook guidebook = guidebookRepository.findById(guidebookId)
+                .orElseThrow(() -> new NoSuchElementException("Guidebook ID: " + guidebookId + " does not exist"));
+        
+        guidebook.increaseView();
+        
+        return GuidebookResponseDto.builder()
+                .userId(guidebook.getUserId())
+                .title(guidebook.getTitle())
+                .content(guidebook.getContent())
+                .region(guidebook.getRegion())
+                .views(guidebook.getViews())
+                .images(guidebook.getImages())
+                .createdAt(guidebook.getCreatedAt())
+                .updatedAt(guidebook.getUpdatedAt())
+                .build();
+    }
+    
+    @Transactional
+    public GuidebookResponseDto updateGuidebook(Long guidebookId, 
+                                     GuidebookRequestDto guidebookRequestDto,
+                                     List<MultipartFile> images) {
+        
+        Optional<Guidebook> guidebookOpt = guidebookRepository.findById(guidebookId);
+        if (guidebookOpt.isEmpty()) {
+            // 검색 실패
+        }
+        
+        Guidebook guidebook = guidebookOpt.get();
+        guidebook.updateGuidebook(guidebookRequestDto, imageService.uploadImages(images));
+        
+        return GuidebookResponseDto.builder()
+                .userId(guidebook.getUserId())
+                .title(guidebook.getTitle())
+                .content(guidebook.getContent())
+                .region(guidebook.getRegion())
+                .views(guidebook.getViews())
+                .images(guidebook.getImages())
+                .createdAt(guidebook.getCreatedAt())
+                .updatedAt(guidebook.getUpdatedAt())
+                .build();
+    }
+    
+    @Transactional
+    public GuidebookResponseDto updateGuidebook(Long guidebookId,
+                                                GuidebookRequestDto guidebookRequestDto) {
+        
+        Optional<Guidebook> guidebookOpt = guidebookRepository.findById(guidebookId);
+        if (guidebookOpt.isEmpty()) {
+            // 검색 실패
+        }
+        
+        Guidebook guidebook = guidebookOpt.get();
+        guidebook.updateGuidebook(guidebookRequestDto);
+        
+        return GuidebookResponseDto.builder()
+                .userId(guidebook.getUserId())
+                .title(guidebook.getTitle())
+                .content(guidebook.getContent())
+                .region(guidebook.getRegion())
+                .views(guidebook.getViews())
+                .createdAt(guidebook.getCreatedAt())
+                .updatedAt(guidebook.getUpdatedAt())
+                .build();
+    }
+    
+    
+    
+    public void deleteGuidebook(Long guidebookId) {
+        guidebookRepository.deleteById(guidebookId);
+    }
+    
+    
+}
