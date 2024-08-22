2964b492d2ff33b88e40197ab2c4e2e3a645dee4
DongUk <az9170@naver.com>
2024-08-22 23:58:50 +0900

    Refactor: 가이드 컨트롤러 메소드 순서 변경

--- src/main/java/trendravel/photoravel_be/domain/guide/controller/GuideController.java
+++ src/main/java/trendravel/photoravel_be/domain/guide/controller/GuideController.java
@@ -1,6 +1,8 @@
 package trendravel.photoravel_be.domain.guide.controller;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import trendravel.photoravel_be.commom.response.Api;
@@ -19,19 +21,8 @@ public class GuideController {
     
     private final GuideService guideService;
     
-    
-    @GetMapping
-    public Api<List<GuideResponseDto>> guidesList(@RequestParam String keyword) {
-
-        return Api.READ(guideService.getGuideList(keyword));
-    }
-    
-    @GetMapping("/{guideId}/detail")
-    public Api<GuideResponseDto> guideDetail(@PathVariable String guideId) {
-
-        return Api.READ(guideService.getGuide(guideId));
-    }
-    
+    @Schema(description = "가이드 회원 가입(CREATE) 요청",
+            contentEncoding = MediaType.APPLICATION_JSON_VALUE)
     @PostMapping("/join")
     public Result createGuide(
             @RequestPart(value = "data") GuideRequestDto guideRequestDto,
@@ -42,15 +33,35 @@ public class GuideController {
         return Result.CREATED();
     }
     
+    @Schema(description = "가이드 로그인 요청",
+            contentEncoding = MediaType.APPLICATION_JSON_VALUE)
     @PostMapping("/login")
     public Result guideLogin(@RequestBody GuideLoginRequestDto loginRequestDto) {
         
-        guideService.authenticate(loginRequestDto.getUsername(), 
+        guideService.authenticate(loginRequestDto.getUsername(),
                 loginRequestDto.getPassword());
-                
+        
         return Result.OK();
     }
     
+    @Schema(description = "가이드 목록 READ 요청",
+            contentEncoding = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping
+    public Api<List<GuideResponseDto>> guidesList(@RequestParam String keyword) {
+        
+        return Api.READ(guideService.getGuideList(keyword));
+    }
+    
+    @Schema(description = "가이드 상세 정보 READ 요청")
+    @GetMapping("/{guideId}/detail")
+    public Api<GuideResponseDto> guideDetail(@PathVariable String guideId) {
+        
+        return Api.READ(guideService.getGuide(guideId));
+    }
+    
+    
+    @Schema(description = "가이드 정보 UPDATE 요청",
+            contentEncoding = MediaType.APPLICATION_JSON_VALUE)
     @PatchMapping("/{guideId}/update")
     public Api<GuideResponseDto> updateGuide(
             @PathVariable String guideId,
@@ -60,6 +71,8 @@ public class GuideController {
         return Api.UPDATED(guideService.updateGuide(guideId, guideRequestDto, images));
     }
     
+    @Schema(description = "가이드 정보 DELETE 요청",
+            contentEncoding = MediaType.APPLICATION_JSON_VALUE)
     @DeleteMapping("/{guideId}/delete")
     public Result guideDelete(@PathVariable String guideId) {
         
