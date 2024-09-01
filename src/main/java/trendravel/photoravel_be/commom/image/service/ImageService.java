e2bcc9f7ca22edb1bbe1e57d39ba9d4aec4484b9
hwangjeyeon <kingpele9018@gmail.com>
2024-09-02 01:07:24 +0900

    Refactor: 이미지 서비스 코드 리팩토링
    - 파사드 패턴 적용
    - 직접 Image Upload, Delete 기능을 사용할 필요가 없게 됨에 따라 파사드 패턴으로 분리
    - 파사드 패턴에서는 Spring event 등록과 ImagePath에 대한 Return 기능만 제공

--- src/main/java/trendravel/photoravel_be/commom/image/service/ImageService.java
+++ src/main/java/trendravel/photoravel_be/commom/image/service/ImageService.java
@@ -1,14 +1,18 @@
 package trendravel.photoravel_be.commom.image.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.*;
 import trendravel.photoravel_be.commom.error.ErrorCode;
+import trendravel.photoravel_be.commom.event.ImageUploadEvent;
+import trendravel.photoravel_be.commom.event.ImageDeleteEvent;
 import trendravel.photoravel_be.commom.exception.ImageSystemException;
 import trendravel.photoravel_be.commom.image.util.ImageNameRebuildUtils;
 
@@ -26,18 +30,40 @@ public class ImageService {
     private final S3Client amazonS3;
     private final S3Client minioClient;
 
+    private final ApplicationEventPublisher eventPublisher;
+
     @Value("${spring.cloud.aws.s3.bucket}")
     private String bucketName;
 
+    @Transactional
+    public List<String> uploadImageFacade(List<MultipartFile> imageFiles) {
+        eventPublisher.publishEvent(new ImageUploadEvent(imageFiles));
+        return getImagesName(imageFiles);
+    }
 
-    public List<String> uploadImages(List<MultipartFile> images){
-
-        if(images == null){
-            return null;
+    @Transactional
+    public List<String> updateImageFacade(List<MultipartFile> newImages,
+                                          List<String> deleteImages){
+        eventPublisher.publishEvent(new ImageDeleteEvent(deleteImages));
+        eventPublisher.publishEvent(new ImageUploadEvent(newImages));
+        if(newImages == null || newImages.isEmpty()){
+            return new ArrayList<>();
         }
 
-//        return uploadImagesAndReturnUrl(images, amazonS3);
-        return uploadImagesAndReturnUrl(images, minioClient);
+        return getImagesName(newImages);
+    }
+
+    @Transactional
+    public void deleteAllImagesFacade(List<String> deleteImages){
+        eventPublisher.publishEvent(new ImageDeleteEvent(deleteImages));
+    }
+
+    public void uploadImages(List<MultipartFile> images){
+        if(images == null){
+            return;
+        }
+        uploadImagesAndReturnUrl(images, minioClient);
+//      uploadImagesAndReturnUrl(images, amazonS3);
     }
 
 
@@ -61,33 +87,28 @@ public class ImageService {
         return rebuildImageName;
     }
 
-    public List<String> updateImages(List<MultipartFile> newImages,
-                                     List<String> deleteImages){
-
-        deleteAllImages(deleteImages);
-
-        if(newImages == null){
-            return new ArrayList<>();
-        }
 
 
-//        return uploadImagesAndReturnUrl(newImages, amazonS3);
-        return uploadImagesAndReturnUrl(newImages, minioClient);
-    }
-
+    /**
+     * db에 들어있는 경우만 지우기!
+     */
 
     public void deleteAllImages(List<String> deleteImages){
+        deleteImagesInStorage(deleteImages, minioClient);
+//        deleteImagesInStorage(deleteImages, amazonS3);
+    }
+
+    private void deleteImagesInStorage(List<String> deleteImages, S3Client storage) {
         List<String> imageNames = sliceUrlAndGetImageNames(deleteImages);
         log.info("{}", imageNames.get(0));
         for (String imageName : imageNames) {
-//            amazonS3.deleteObject(bucketName, imageName);
             log.info("{}", imageName);
             DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest
                     .builder()
                     .bucket(bucketName)
                     .key(imageName)
                     .build();
-            minioClient.deleteObject(deleteObjectRequest);
+            storage.deleteObject(deleteObjectRequest);
         }
     }
 
@@ -98,7 +119,7 @@ public class ImageService {
     }
 
 
-    public List<String> getImagesName(List<MultipartFile> multipartFiles){
+    private List<String> getImagesName(List<MultipartFile> multipartFiles){
         return multipartFiles.stream()
                 .map(p-> ImageNameRebuildUtils
                         .buildImageName(p.getOriginalFilename()))
