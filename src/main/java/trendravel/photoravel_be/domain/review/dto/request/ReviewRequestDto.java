cb841426642204787bd6a54e567d2d7beadbe669
hwangjeyeon <kingpele9018@gmail.com>
2024-09-02 23:52:36 +0900

    Refactor: REVIEW 도메인 검증위치 변경에 따른 DTO로의 검증 기능 추가

--- src/main/java/trendravel/photoravel_be/domain/review/dto/request/ReviewRequestDto.java
+++ src/main/java/trendravel/photoravel_be/domain/review/dto/request/ReviewRequestDto.java
@@ -2,7 +2,11 @@ package trendravel.photoravel_be.domain.review.dto.request;
 
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.http.MediaType;
 import trendravel.photoravel_be.db.review.enums.ReviewTypes;
 
@@ -18,8 +22,12 @@ public class ReviewRequestDto {
     @Schema(description = "리뷰타입")
     private ReviewTypes reviewType;
     @Schema(description = "리뷰내용")
+    @NotBlank(message = "공백/null 입력은 미허용됩니다.")
+    @Length(max = 500, message = "최대 길이는 500자 입니다.")
     private String content;
     @Schema(description = "리뷰평점")
+    @DecimalMax(value = "5.0", message = "최대 허용 별점은 5.0입니다.")
+    @DecimalMin(value = "1.0", message = "최소 허용 별점은 1.0입니다.")
     private double rating;
     @Schema(description = "유저아이디")
     private String userId;
