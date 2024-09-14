303598daab09e80187a0e34e5b97e47898895e06
hwangjeyeon <kingpele9018@gmail.com>
2024-09-14 20:16:06 +0900

    Feat: MemberEntity 도메인과 연관관계 설정, null 관련설정 추가
    - Merge된 MemberEntity도메인과 연관관계 설정
    - null값 미허용 설정 추가
    - 조회수 초기값 0으로 설정

--- src/main/java/trendravel/photoravel_be/db/location/Location.java
+++ src/main/java/trendravel/photoravel_be/db/location/Location.java
@@ -2,14 +2,12 @@ package trendravel.photoravel_be.db.location;
 
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.*;
-import org.hibernate.validator.constraints.Length;
+import org.hibernate.annotations.ColumnDefault;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import trendravel.photoravel_be.db.BaseEntity;
+import trendravel.photoravel_be.db.member.MemberEntity;
 import trendravel.photoravel_be.domain.location.dto.request.LocationRequestDto;
 import trendravel.photoravel_be.db.review.Review;
 import trendravel.photoravel_be.db.spot.Spot;
@@ -35,15 +33,17 @@ public class Location extends BaseEntity {
     @Column(name = "location_id")
     private Long id;
 
+    @Column(nullable = false)
     private Double latitude;
+    @Column(nullable = false)
     private Double longitude;
-    @Column(length = 50)
+    @Column(length = 50, nullable = false)
     private String address;
 
-    @Column(columnDefinition = "TEXT")
+    @Column(columnDefinition = "TEXT", nullable = false)
     private String description;
 
-    @Column(length = 50)
+    @Column(length = 50, nullable = false)
     private String name;
 
     @Column(columnDefinition = "POINT SRID 4326", nullable = false)
@@ -56,6 +56,8 @@ public class Location extends BaseEntity {
     )
     @Builder.Default
     private List<String> images = new ArrayList<>();
+    @Column(nullable = false)
+    @ColumnDefault("0")
     private int views;
 
     //유저 엔티티 생성 후, 연관관계 필드 추가 필요
@@ -69,6 +71,15 @@ public class Location extends BaseEntity {
     @Builder.Default
     private List<Review> review = new ArrayList<>();
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "memberEntity_id")
+    private MemberEntity member;
+
+
+    public void setMemberLocation(MemberEntity member) {
+        this.member = member;
+        member.getLocationMember().add(this);
+    }
 
     public void updateLocation(LocationUpdateImagesDto location, List<String> newImages){
         this.longitude = location.getLongitude();
