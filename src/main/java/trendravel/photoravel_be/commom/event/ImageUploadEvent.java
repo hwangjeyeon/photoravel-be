5a2b25a7850dc3becad9d0ef8a49d47d70692dae
hwangjeyeon <kingpele9018@gmail.com>
2024-09-14 23:12:34 +0900

    Fix: DB에 저장되는 객체명과 S3에 저장되는 객체명 불일치 문제 해결
    - DB에 저장할때 파일명 생성하고, S3 저장할 때 또다시 파일명 생성 후 저장하는 로직때문에 발생한 문제
    - S3 저장할 때 파일명 생성하는 로직 삭제
    - DB에 저장할 때 생성된 파일명을 이벤트에 넘겨주도록 코드 변경

--- src/main/java/trendravel/photoravel_be/commom/event/ImageUploadEvent.java
+++ src/main/java/trendravel/photoravel_be/commom/event/ImageUploadEvent.java
@@ -5,11 +5,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class ImageUploadEvent {
-    private List<MultipartFile> uploadImages = new ArrayList<>();
+    private List<MultipartFile> uploadImages;
+    private List<String> rebuildImageName;
 }
