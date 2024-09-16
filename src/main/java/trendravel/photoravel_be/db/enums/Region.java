6f5e5604225f816c0a323190b43b1028cc7e6afb
DongUk <az9170@naver.com>
2024-09-16 20:17:57 +0900

    Feat: 충청남도 지역 enum 추가

--- src/main/java/trendravel/photoravel_be/db/enums/Region.java
+++ src/main/java/trendravel/photoravel_be/db/enums/Region.java
@@ -2,5 +2,18 @@ package trendravel.photoravel_be.db.enums;
 
 public enum Region {
     아산, 
-    천안
+    천안,
+    계룡,
+    공주,
+    논산,
+    당진,
+    보령,
+    서산,
+    금산,
+    부여,
+    서천,
+    예산,
+    청양,
+    태안,
+    홍성
 }
