a666fad071d9ac171720f7967bda0d35672dd0e6
hwangjeyeon <kingpele9018@gmail.com>
2024-09-16 23:13:05 +0900

    Config: 운영/개발(로컬) 서버 프로필 설정에 따른 설정 변경
    - 로컬 환경용 S3Config 파일 추가 생성

--- src/main/java/trendravel/photoravel_be/config/S3ConfigInLocal.java
+++ src/main/java/trendravel/photoravel_be/config/S3ConfigInLocal.java
@@ -5,39 +5,42 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import software.amazon.awssdk.auth.credentials.*;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 import java.net.URI;
 
 @Configuration
-@Profile("prod")
-public class S3Config {
-
-    @Value("${spring.cloud.aws.credentials.access-key}")
-    private String accessKey;
-
-    @Value("${spring.cloud.aws.credentials.secret-key}")
-    private String accessSecret;
+@Profile({"dev", "test"})
+public class S3ConfigInLocal extends S3Config{
 
     @Value("${spring.cloud.aws.region.static}")
     private String region;
 
+    @Value("${minio.access.key}")
+    private String minioAccessKey;
+
+    @Value("${minio.access.secret}")
+    private String minioSecretKey;
+
+    @Value("${minio.url}")
+    private String minioUrl;
+
+
 
     @Bean
-    public S3Client amazonS3(){
+    public S3Client minioClient(){
         return S3Client.builder()
+                .endpointOverride(URI.create(minioUrl))
                 .region(Region.of(region))
                 .credentialsProvider(StaticCredentialsProvider
                         .create(AwsBasicCredentials
-                                .create(accessKey, accessSecret)))
+                                .create(minioAccessKey, minioSecretKey)))
+                .forcePathStyle(true)
                 .build();
     }
-
-
-
-
 
 
 
