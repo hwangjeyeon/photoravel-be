5db2790755a3af6a92fa13f795db89589ec632c9
Seong57 <gustjr7297@naver.com>
2024-09-23 16:53:52 +0900

    Refactor: private Controller 분리

--- src/main/java/trendravel/photoravel_be/domain/photographer/controller/PrivatePhotographerController.java
+++ src/main/java/trendravel/photoravel_be/domain/photographer/controller/PrivatePhotographerController.java
@@ -7,61 +7,20 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import trendravel.photoravel_be.commom.response.Api;
 import trendravel.photoravel_be.commom.response.Result;
-import trendravel.photoravel_be.domain.photographer.dto.request.PhotographerLoginRequestDto;
-import trendravel.photoravel_be.domain.photographer.dto.request.PhotographerRequestDto;
 import trendravel.photoravel_be.domain.photographer.dto.request.PhotographerUpdateDto;
-import trendravel.photoravel_be.domain.photographer.dto.response.PhotographerListResponseDto;
 import trendravel.photoravel_be.domain.photographer.dto.response.PhotographerSingleResponseDto;
 import trendravel.photoravel_be.domain.photographer.service.PhotographerService;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/photographers")
+@RequestMapping("/private/photographers")
 @RequiredArgsConstructor
-public class PhotographerController {
+public class PrivatePhotographerController {
     
     private final PhotographerService photographerService;
     
-    @Schema(description = "사진작가 회원 가입(CREATE) 요청",
-            contentEncoding = MediaType.MULTIPART_FORM_DATA_VALUE)
-    @PostMapping(value = "/public/join",
-            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public Result createPhotographer(
-            @RequestPart(value = "data") PhotographerRequestDto photographerRequestDto,
-            @RequestPart(value = "image", required = false) List<MultipartFile> images) {
-        
-        photographerService.createPhotographer(photographerRequestDto, images);
-        
-        return Result.CREATED();
-    }
-    
-    @Schema(description = "사진작가 로그인 요청",
-            contentEncoding = MediaType.APPLICATION_JSON_VALUE)
-    @PostMapping("/public/login")
-    public Result loginPhotographer(@RequestBody PhotographerLoginRequestDto loginRequestDto) {
-        
-        photographerService.authenticate(loginRequestDto.getUsername(),
-                loginRequestDto.getPassword());
-        
-        return Result.OK();
-    }
-    
-    @Schema(description = "사진작가 목록 READ 요청",
-            contentEncoding = MediaType.APPLICATION_JSON_VALUE)
-    @GetMapping("/public")
-    public Api<List<PhotographerListResponseDto>> getPhotographerList(@RequestParam String region) {
-        
-        return Api.READ(photographerService.getPhotographerList(region));
-    }
-    
-    @Schema(description = "사진작가 상세 정보 READ 요청")
-    @GetMapping("/public/{photographerId}/detail")
-    public Api<PhotographerSingleResponseDto> getPhotographer(@PathVariable String photographerId) {
-        
-        return Api.READ(photographerService.getPhotographer(photographerId));
-    }
+
     
     
     @Schema(description = "사진작가 정보 UPDATE 요청 (이미지 미포함)",
