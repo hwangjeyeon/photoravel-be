37c0134486280bfc8a0efa08bc81ddcd9a6c800e
Seong57 <gustjr7297@naver.com>
2024-09-29 08:24:51 +0900

    Refactor: payload에 role 추가 및 사진작가 토큰 발급 기능 구현

--- src/main/java/trendravel/photoravel_be/domain/token/service/TokenService.java
+++ src/main/java/trendravel/photoravel_be/domain/token/service/TokenService.java
@@ -6,9 +6,12 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import trendravel.photoravel_be.commom.error.ErrorCode;
 import trendravel.photoravel_be.commom.error.MemberErrorCode;
+import trendravel.photoravel_be.commom.error.PhotographerErrorCode;
 import trendravel.photoravel_be.commom.exception.ApiException;
 import trendravel.photoravel_be.db.member.MemberEntity;
+import trendravel.photoravel_be.db.photographer.Photographer;
 import trendravel.photoravel_be.db.respository.member.MemberRepository;
+import trendravel.photoravel_be.db.respository.photographer.PhotographerRepository;
 import trendravel.photoravel_be.domain.token.helper.TokenHelper;
 import trendravel.photoravel_be.db.inmemorydb.entity.Token;
 import trendravel.photoravel_be.domain.token.model.TokenDto;
@@ -24,6 +27,7 @@ public class TokenService {
 
     private final TokenHelper tokenHelper;
     private final MemberRepository memberRepository;
+    private final PhotographerRepository photographerRepository;
     private final RedisTemplate<String, Token> redisTemplate;
 
     public TokenDto issueAccessToken(String memberId) {
@@ -35,6 +39,7 @@ public class TokenService {
         data.put("name", member.getName());
         data.put("nickname", member.getNickname());
         data.put("memberId", member.getMemberId());
+        data.put("role", "member");
         data.put("issuer", "photoravel");
         return tokenHelper.issueAccessToken(data);
     }
@@ -47,6 +52,7 @@ public class TokenService {
         data.put("name", member.getName());
         data.put("nickname", member.getNickname());
         data.put("memberId", member.getMemberId());
+        data.put("role", "member");
         data.put("issuer", "photoravel");
         TokenDto refreshToken = tokenHelper.issueRefreshToken(  data);
         saveRefreshToken(refreshToken, member.getMemberId());
@@ -54,14 +60,36 @@ public class TokenService {
         return refreshToken;
     }
 
-    public String validationTokenWithEmail(String token){
+    public TokenDto issuePhotographerAccessToken(String accountId) {
+        Photographer photographer = photographerRepository.findByAccountId(accountId)
+                .orElseThrow(() -> new ApiException(PhotographerErrorCode.PHOTOGRAPHER_NOT_FOUND));
 
-        Map<String, Object> data = tokenHelper.validationTokenWithThrow(token);
-        Object userEmail = data.get("email");
-        Objects.requireNonNull(userEmail, () -> {
-            throw new ApiException(ErrorCode.NULL_POINT);
-        });
-        return userEmail.toString();
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("accountId", photographer.getAccountId());
+        data.put("name", photographer.getName());
+        data.put("role", "photographer");
+        data.put("issuer", "photoravel");
+        return tokenHelper.issueAccessToken(data);
+    }
+
+    public TokenDto issuePhotographerRefreshToken(String accountId) {
+        Photographer photographer = photographerRepository.findByAccountId(accountId)
+                .orElseThrow(() -> new ApiException(PhotographerErrorCode.PHOTOGRAPHER_NOT_FOUND));
+
+        HashMap<String, Object> data = new HashMap<>();
+        data.put("accountId", photographer.getAccountId());
+        data.put("name", photographer.getName());
+        data.put("role", "photographer");
+        data.put("issuer", "photoravel");
+        TokenDto refreshToken = tokenHelper.issueRefreshToken(data);
+        savePhotographerRefreshToken(refreshToken, photographer.getAccountId());
+
+        return refreshToken;
+    }
+
+    public Map<String, Object> validationTokenWithMap(String token){
+
+        return tokenHelper.validationTokenWithThrow(token);
     }
 
     public String validationTokenWithMemberId(String token){
@@ -74,13 +102,34 @@ public class TokenService {
         return memberId.toString();
     }
 
+    public String validationTokenWithAccountId(String token){
+
+        Map<String, Object> data = tokenHelper.validationTokenWithThrow(token);
+        Object accountId = data.get("accountId");
+        Objects.requireNonNull(accountId, () -> {
+            throw new ApiException(ErrorCode.NULL_POINT);
+        });
+        return accountId.toString();
+    }
+
+
     public Token saveRefreshToken(TokenDto tokendto, String memberId) {
         Token token = Token.builder()
                 .id(memberId)
                 .refreshToken(tokendto.getToken())
                 .build();
 
-        redisTemplate.opsForHash().put("refresh_token", token.getId(), token);
+        redisTemplate.opsForHash().put("member_refresh_token", token.getId(), token);
+        return token;
+    }
+
+    public Token savePhotographerRefreshToken(TokenDto tokendto, String accountId) {
+        Token token = Token.builder()
+                .id(accountId)
+                .refreshToken(tokendto.getToken())
+                .build();
+
+        redisTemplate.opsForHash().put("photographer_refresh_token", token.getId(), token);
         return token;
     }
 
