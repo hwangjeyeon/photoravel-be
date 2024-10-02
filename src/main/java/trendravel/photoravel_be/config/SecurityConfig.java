d16ebd6f0d31b13b2ef7a2e4a1edbb25913a22ab
Seong57 <gustjr7297@naver.com>
2024-10-02 13:20:37 +0900

    refactor: cors 수정

--- src/main/java/trendravel/photoravel_be/config/SecurityConfig.java
+++ src/main/java/trendravel/photoravel_be/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        config.setAllowedOrigins(Collections.singletonList("http://photoravel-fe.s3-website.ap-northeast-2.amazonaws.com"));
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
