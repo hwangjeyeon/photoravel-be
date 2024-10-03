ae7869d8080089e5efc98311ce3d199a95d2cadb
Seong57 <gustjr7297@naver.com>
2024-10-03 17:11:16 +0900

    fix: loaclhost:80 cors 허용

--- src/main/java/trendravel/photoravel_be/config/SecurityConfig.java
+++ src/main/java/trendravel/photoravel_be/config/SecurityConfig.java
@@ -51,7 +51,7 @@ public class SecurityConfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(List.of("http://52.219.148.60:80", "http://photoravel-fe.s3-website.ap-northeast-2.amazonaws.com:80", "http://localhost:80"));
+                        config.setAllowedOrigins(List.of("http://photoravel-fe.s3-website.ap-northeast-2.amazonaws.com", "http://localhost"));
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
